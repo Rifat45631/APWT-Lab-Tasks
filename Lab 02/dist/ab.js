41b8532d329fdb5d12c2c9a1198cf9a9e21da836
Rifat Arafin <152815955+Rifat45631@users.noreply.github.com>
2025-04-25 23:18:47 +0600

    Delete Lab 02 directory

--- Lab 02/dist/ab.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let a = 10;
-if (a < 18) {
-    console.log("hello");
-}
-else
-    console.log("holllaaa");
-//# sourceMappingURL=ab.js.map
--- Lab 02/src/ab.ts
+++
@@ -1,8 +0,0 @@
-let a: number=10
-if(a<18)
-{
-    console.log("hello");
-    
-}
-else 
-console.log("holllaaa");
